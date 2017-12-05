d17c5398c4d4d76e939893d7e42d4e0eba70b5fe
Esteban Cardona <bootcamp@H5661.ad.hugeinc.com>
2017-12-05 16:14:26 -0500

    Created gallery with js, still working with dots and keyboard

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,95 +1,78 @@
 export class Gallery {
-    constructor(element,img){
-        //Set Element and initial position
-        this.node = document.querySelector(element);
-        this.index = 0;
-        
-        this.setGalleryStructure();
-        // this.setArrows();
-        // this.setDots();
-        // this.fillGallery(img);
-        // this.setBounds();
-    }
+  constructor (element, img) {
+        // Set Element and initial position
+    this.node = document.querySelector(element)
+    this.index = 0
+    this.elements = {}
 
-    static get galleryStructure(){
-        return {
-            arrows:(
+    this.setGalleryStructure()
+    this.fillGallery(img)
+    this.setArrows()
+    this.setDots()
+    // this.setBounds();
+  }
+
+  static get galleryStructure () {
+    return {
+      arrows: (
                 `<div class="arrow arrow__left">
                 </div>
                 <div class="arrow arrow__right">
-                </div>`)
-        }
+                </div>`),
+      gallery: (
+                `<img src="{src}" alt="" class="gallery__image hidden" data-index="{index}">`
+            ),
+      dots: (
+        `<div class="dots">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        </div>`
+    )
     }
+  }
 
-    setGalleryStructure(){
-        this.node.innerHTML = Gallery.galleryStructure.arrows;
-        this.node.tabIndex = 0;
-        console.log('melo');
+  setGalleryStructure () {
+    this.node.innerHTML = Gallery.galleryStructure.arrows + Gallery.galleryStructure.dots
+    this.node.tabIndex = 0
+  }
+
+  setArrows () {
+    this.elements.leftArrow = this.node.querySelector('.arrow__left')
+    this.elements.rightArrow = this.node.querySelector('.arrow__right')
+
+    this.elements.leftArrow.addEventListener('click', this.prev.bind(this))
+    this.elements.rightArrow.addEventListener('click', this.next.bind(this))
+  }
+
+  next () {
+    if (this.index < this.elements.gallery.length - 1) {
+      this.reveal(this.index, ++this.index)
     }
-
-    static setArrows(){
-        // const gallery = document.querySelector('.gallery');
-        // const arrows = [document.createElement('div'), document.createElement('div')]
-        // arrows[0].classList.add('arrow','arrow__left');
-        // arrows[1].classList.add('arrow','arrow__right');
-        addEvents(arrows);
-        arrows.forEach(element => {
-            gallery.appendChild(element);
-        });
+  }
+  prev () {
+    if (this.index > 0) {
+      this.reveal(this.index, --this.index)
     }
-    
+  }
 
-    // //alksdjdbasldkjbasldkjbaslkdjbsaldkjbaslkjdba
-    // function addEvents(arrows) {
-    //     const img = iterator(getImages());
-    //     arrows[0].addEventListener('click', () =>{
-    //         prev(img);
-    //     });
-    //     arrows[1].addEventListener('click', () =>{
-    //         next(img);
-    //     });
-    // }
-    
-    // function getImages() {
-    //     return document.querySelectorAll('img.gallery__image');
-    // }
-    
-    // const iterator = function (img) {
-    //     let pos = 0;
-    //     img.next = (function () {
-    //         return (pos++ >= this.length -1) ? false : this[pos];
-    //     });
-    //     img.prev = (function () {
-    //         return (pos-- <= 1) ? false : this[pos];
-    //     });
-    //     return img;
-    // };
-    
-    // function prev(img) {
-    //     const actualImg = img.prev();
-    //     actualImg.classList.remove('hidden');
-    //     img.forEach(element => {
-    //         if(element != actualImg)
-    //         element.classList.add('hidden');
-    //     });
-    // }
-    // function next(img) {
-    //     const actualImg = img.next();
-    //     actualImg.classList.remove('hidden');
-    //     img.forEach(element => {
-    //         if(element != actualImg)
-    //         element.classList.add('hidden');
-    //     });
-    // }
-    
-    // function reveal(pos, next) {
-    //     if(next){
-    //         img[pos--].classList.add('hidden');
-    //         img[pos].classList.remove('hidden');
-    //     }else{
-    //         img[pos].classList.add('hidden');
-    //         img[pos++].classList.remove('hidden');
-    //     }
-    // }
+  fillGallery (data) {
+    data.map(element => {
+      this.node.innerHTML += Gallery.galleryStructure.gallery.replace('{src}', element.url)
+    })
+    this.elements.gallery = this.node.querySelectorAll('.gallery__image')
+    this.reveal(0, 0)
+  }
+
+  reveal (hide, reveal) {
+    // console.log(`reveal = ${hide}, ${reveal}`)
+    this.elements.gallery[hide].classList.add('hidden')
+    this.elements.gallery[reveal].classList.remove('hidden')
+  }
+
+  setDots () {
+    this.elements.dots = this.node.querySelectorAll('.dots')
+  }
 }
-    
+
+// Falta agregar dots con el indice correspondiente y ubicar de acuerdo a su imagen, para que al momento de hacer el
+// click, se haga la transicion.
